78a28a4c845630ae9242358ec82be70d19fc7254
tournevis <arthur.ro92@gmail.com>
2017-10-29 19:13:16 +0100

    💎  first step of rework bot

---
+++ main.js
@@ -0,0 +1,45 @@
+'use strict'
+const Twit = require('twit')
+const key = require('./config/config.json')
+const blacklist = require('./config/blacklist.json')
+const Botvember = require('./scripts/Botvember.js')
+
+// Fetch the service account key JSON file contents
+const serviceAccount = require('./config/database.json')
+const bot = new Botvember(serviceAccount, 'https://codevember-f16a5.firebaseio.com/', 2017)
+bot.init()
+
+const T = new Twit({
+  consumer_key: key.consumer_key,
+  consumer_secret: key.consumer_secret,
+  access_token: key.access_token,
+  access_token_secret: key.access_token_secret
+})
+
+const filter = '#codevember'
+
+let stream = T.stream('statuses/filter', { track: filter })
+console.log(`Stream Started : ${filter}`)
+stream.on('tweet', (tweet) => {
+  var blacklisted = false
+  blacklist.users.forEach((user) => {
+    if (user === tweet.user.screen_name) {
+      blacklisted = true
+    }
+  })
+  if (!tweet.retweeted && tweet.entities.urls[0] && !blacklisted) {
+    bot.getCodepenData(tweet, res => {
+      console.log(res)
+      favTweet(tweet.id_str)
+    }, err => {
+      console.log(err)
+    })
+  }
+})
+
+function favTweet (tweetId) {
+  T.post('favorites/create', { id: tweetId }, function (err, data, response) {
+    err && console.log(err)
+    response && console.log('Tw Fav !')
+  })
+}
---
+++ scripts/Botvember.js
@@ -0,0 +1,136 @@
+'use strict'
+const phantom = require('phantom')
+const admin = require('firebase-admin')
+class Botvember {
+  constructor (serviceAccount, databaseUrl, year) {
+    this.serviceAccount = serviceAccount
+    this.databaseUrl = databaseUrl
+    this.regex = /(https:\/\/www\.)?codepen\.io\/([a-zA-Z0-9@:%_]*)\/(pen|full)?\/([a-zA-Z0-9@:%_]*)/gim
+    this.codepenAPI = 'http://codepen.io/'
+    this.year = year
+  }
+
+  init () {
+    admin.initializeApp({
+      credential: admin.credential.cert(this.serviceAccount),
+      databaseURL: this.databaseUrl
+    })
+    this.db = admin.database()
+    this.ref = this.db.ref(`${this.year}/contributions`)
+  }
+  isCodepenLink (tweet, res, err) {
+    let penData = []
+    for (var i = 0; i < tweet.entities.urls.length; i++) {
+      if( this.expendUrl(tweet.entities.urls[i].expanded_url)) {
+        penData.push(this.getPenData())
+      }
+    }
+    if (penData.length > 0) {
+      penData.screen_name = tweet.user.screen_name
+      typeof res === 'function' && res()
+      // penData.penDetails && createDocument(penData)
+    } else {
+      typeof err === 'function' && err()
+    }
+  }
+  expendUrl (url) {
+    var regResult = this.regex.exec(url)
+    if (regResult != null) {
+      return regResult
+    } else {
+      return null
+    }
+  }
+  getPenData (penData) {
+    let data = {}
+    data.user = penData[2]
+    data.pen = penData[4]
+    data.penUrl = `${this.codepenAPI}${data.user}/pen/${data.pen}`
+    data.fullUrl = `${this.codepenAPI}${data.user}/full/${data.pen}`
+    data.imgUrl = `${this.codepenAPI}${data.user}/pen/${data.pen}/image/small.png`
+    data.penDetails = `${this.codepenAPI}${data.user}/details/${data.pen}`
+    return data
+  }
+  createDocument (data) {
+    let phInstance = null
+    let phPage = null
+    phantom.create()
+      .then(instance => {
+        phInstance = instance
+        return instance.createPage()
+      })
+      .then(page => {
+        phPage = page
+        return page.open(data.penDetails)
+      })
+      .then(status => {
+        return phPage.property('content')
+      })
+      .then(generated => {
+        phPage.evaluate(function () {
+          return [
+            document.getElementsByClassName('pen-title-link')[0].innerHTML.trim(),
+            document.querySelector('time').innerHTML
+          ]
+        }).then(html => {
+          data.title = html[0]
+          data.date = html[1].trim().split(/\s* \s*/)
+          data.date[1] = data.date[1].slice(0, -1)
+          data.day = parseInt(data.date[1], 10)
+          var year = parseInt(data.date[2], 10)
+          if (year === this.year) {
+            this.postDb(data)
+            phInstance.exit()
+          } else {
+            phInstance.exit()
+          }
+        })
+      })
+      .catch(error => {
+        console.log(error)
+        phInstance.exit()
+      })
+  }
+  postDb (data) {
+    this.existInBase(data).then((data) => {
+      var newContrib = this.ref.push()
+      newContrib.set({
+        'author': data.screen_name,
+        'day': data.day,
+        'slug': this.generateSlug(data),
+        'image': data.imgUrl,
+        'title': data.title,
+        'url': data.fullUrl
+      })
+      console.log(`Added ${data.title} on ${data.day}, by ${data.screen_name}`)
+    }, (rej) => {
+      console.log(`Already in Database ${rej}`)
+    }).catch((err) => {
+      console.log(`Catch an Error : ${err}`)
+    })
+  }
+  existInBase (data) {
+    return new Promise((resolve, reject) => {
+      this.ref.once('value').then((snapshot) => {
+        snapshot.forEach((dataDb) => {
+          if (data.fullUrl === dataDb.val().url) {
+            reject('Already in base')
+          }
+        })
+        resolve(data)
+      })
+    })
+  }
+  generateSlug (data) {
+    let slug = ''
+    let day = data.day
+    if (day < 10) {
+      slug = '0'
+    }
+    slug += day + ' ' + data.title + ' ' + data.screen_name
+    slug = slug.replace(/\s/g, '-').toLowerCase()
+    return slug
+  }
+}
+
+module.exports = Botvember
